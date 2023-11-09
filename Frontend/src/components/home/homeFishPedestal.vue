4c57d4035caea2ff24b423ce5e18e269b82762c6
HuyB2012094 <110045881+GiaHuyB2012094@users.noreply.github.com>
2023-11-09 22:45:52 +0700

    Update homeFishPedestal.vue

--- Frontend/src/components/home/homeFishPedestal.vue
+++ Frontend/src/components/home/homeFishPedestal.vue
@@ -41,7 +41,7 @@ import axios from 'axios';
     methods: {
         async getListFishPedestal(){
             try {
-                const result = await axios.get('http://localhost:4000/api/fishpedestal/home/list')
+                const result = await axios.get('web-ocean-business-api.vercel.app/api/fishpedestal/home/list')
                 this.listFishPedestal = result.data
             }catch(err){
                 console.log(err)
